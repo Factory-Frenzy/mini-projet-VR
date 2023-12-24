9169b7c3bbd4dfa00e017430c37c7531fa78d568
Arnaud <arnaud.dellam@gmail.com>
2023-12-24 17:33:07 +0100

    F8 : Créer une caméra spectatrice à partir de la FlyCam fournie en unitypackage pour permettre au formateur de prendre un autre point de vue que celui de l’utilisateur·rice (utliser la touche "C" du clavier pour changer de vue)
    +
    
    Tete layer:grabbeable

--- Assets/FlyCam/FlyCam.cs
+++ Assets/FlyCam/FlyCam.cs
@@ -1,139 +1,58 @@
-// This script was automatically created by script templates stored at C:\Program Files\Unity\Hub\Editor\{UnityVersion}\Editor\Data\Resources\ScriptTemplates
-// Script created by LINEACT CESI
-// Author: FlyCam
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace intervales.utils
 {
     public class FlyCam : MonoBehaviour
     {
-        #region own property, use camelLowerCase for private property, use CamelUpperCase for public property 
-        private Vector2 look;
-        private Vector2 move;
-        private InputAction fire2Action, fire3Action, moveFastAction;
+        public float mouseSensitivity = 100f;
+        public Camera firstCamera;
+        public Camera secondCamera;
 
-        public Camera cam;
+        private float xRotation = 0f;
+        private float yRotation = 0f;
 
-        public float RotXSensitivity = 1.0f;
-        public float RotYSensitivity = 1.0f;
-
-        public float MoveXSensitivity = 1.0f;
-        public float MoveYSensitivity = 1.0f;
-
-
-        public float AltXSensitivity = 1.0f;
-        public float AltYSensitivity = 1.0f;
-        public float MoveFastMultiplier = 2.0f;
-
-        public InputActionAsset inputActions;
-        #endregion
-
-
-
-        #region Unity Events
-        // Start is called before the first frame update
         void Start()
         {
-            if (inputActions == null) inputActions = GetComponent<PlayerInput>().actions;
-            look = Vector2.zero;
-            cam = transform.Find("Camera").gameObject.GetComponent<Camera>();
-
-
-            fire2Action = inputActions.FindAction("Fire2");
-            fire3Action = inputActions.FindAction("Fire3");
-            moveFastAction = inputActions.FindAction("MoveFast");
-
+            // Lock the cursor to the center of the screen and make it invisible
+            Cursor.lockState = CursorLockMode.Locked;
+            firstCamera.enabled = true;
+            secondCamera.enabled = false;
         }
 
-        //var action = inputActions.FindAction("Fire");
-        //// Have it run your code when the Action is triggered.
-        //action.performed += Fire_performed;
-        //// Start listening for control changes.
-        //action.Enable();
-        //private void Fire_performed(InputAction.CallbackContext obj)
-        //{
-        //    Debug.Log($"Fire {obj}");
-        //}
-
-        
-
-        // Update is called once per frame
         void Update()
         {
-            
-        }
-
-        void FixedUpdate()
-        {
-            //Debug.Log($"Fire {inputActions.FindAction("Fire").IsPressed()}");
-            //if(cam != null && look != Vector2.zero && Mouse.current.rightButton.isPressed)
-            float multiplier = moveFastAction.IsPressed() ? MoveFastMultiplier : 1;
-            if (cam != null && look != Vector2.zero && fire2Action.IsPressed())
+            if (secondCamera.enabled)
             {
-                var v = Vector3.zero;
-                var xAngle = -look.y * RotXSensitivity *multiplier *  Time.fixedDeltaTime;
-                var yAngle = look.x * RotYSensitivity * multiplier * Time.fixedDeltaTime;
+                // Get mouse input
+                float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+                float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-                // Free Look
-                transform.Rotate(0, yAngle, 0, Space.Self);
-                cam.transform.Rotate(xAngle, 0, 0, Space.Self);
-                //cam.transform.rotation *= Quaternion.Euler(v);
-                //cam.transform.eulerAngles += v;
+                // Apply vertical rotation (pitch)
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-                /*transform.rotation *= Quaternion.FromToRotation(cam.transform.forward, (cam.transform.forward +
-                    cam.transform.right * look.x * RotYSensitivity * Time.fixedDeltaTime + 
-                    cam.transform.up * look.y * RotXSensitivity * Time.fixedDeltaTime).normalized );*/
-            }
-            
-            
+                // Apply horizontal rotation (yaw)
+                yRotation += mouseX;
+                yRotation = yRotation % 360; // Optional: keeps the rotation value from overflowing
 
-            if (cam != null && look != Vector2.zero && fire3Action.IsPressed())
-            {
-                var v = Vector3.zero;
+                // Apply the rotations to the camera
+                transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
 
-                v.x = -look.x * AltXSensitivity* multiplier * Time.fixedDeltaTime;
-                v.y = -look.y * AltYSensitivity * multiplier * Time.fixedDeltaTime;
+                // Optionally, align the player body's y rotation with the camera's y rotation
+                //playerBody.rotation = Quaternion.Euler(0f, yRotation, 0f);
 
-                v = cam.transform.TransformDirection(v);
-                transform.position += v;
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+                }
             }
 
-            if (cam != null && move != Vector2.zero)
+            if (Input.GetKeyDown(KeyCode.C)) // Change 'C' to your preferred key
             {
-                var v = Vector3.zero;
-                
-                v.x = move.x * MoveXSensitivity * multiplier * Time.fixedDeltaTime;
-                v.z = move.y * MoveYSensitivity * multiplier * Time.fixedDeltaTime;
-                
-                v = cam.transform.TransformDirection(v);
-                transform.position += v;
+                // Toggle the enabled state of the cameras
+                firstCamera.enabled = !firstCamera.enabled;
+                secondCamera.enabled = !secondCamera.enabled;
             }
         }
-        #endregion Unity Events
-
-
-        #region own methods, use camelLowerCase for private method name, use CamelUpperCase for public method name 
-
-        #endregion
-
-        #region own events
-        public void OnLook(InputValue value)
-        {
-            // Give the delta between previous value
-            look = value.Get<Vector2>();
-            //Debug.Log(string.Format("OnLook {0}", look));
-        }
-
-        public void OnMove(InputValue value)
-        {
-            move = value.Get<Vector2>();
-            //Debug.Log(string.Format($"OnMove {move}"));
-        }
-
-        #endregion
     }
 }
